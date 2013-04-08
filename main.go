07046662be255d5033ae8de6e98336d816d88148
Jasdeep Madan <jasdeepm@gmail.com>
2013-04-08 19:53:51 +0100

    adding back main.go

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"code.google.com/p/gorest"
+	Authenticate "msgstory/authenticate"
+	Register "msgstory/register"
+	User "msgstory/user"
+	"net/http"
+)
+
+type person struct {
+	Name  string
+	Email string
+}
+
+type AppService struct {
+	gorest.RestService `root:"/"`
+	getApp             gorest.EndPoint `method:"GET" path:"/" output:"string"`
+}
+
+type UserService struct {
+	gorest.RestService `root:"/user-service/"`
+
+	getUser gorest.EndPoint `method:"GET" path:"/getuser/{name:string}" output:"string"`
+}
+
+type MsgService struct {
+	gorest.RestService `root:"/getmessage/"`
+
+	getMessage gorest.EndPoint `method:"GET" path:"/get-message" output:"string"`
+	getLogin   gorest.EndPoint `method:"GET" path:"/login" output:"string"`
+}
+
+type RegisterService struct {
+	gorest.RestService `root:"/create/"`
+	// createUser         gorest.EndPoint `method:"POST" path:"/new/{uemail:string}/{pass:string}" postdata:"person"`
+	createUser gorest.EndPoint `method:"GET" path:"/new/{uemail:string}/{pass:string}" output:"string"`
+}
+
+func (serv RegisterService) CreateUser(uemail, pass string) string {
+	Register.Register(uemail, pass)
+	return "Executed!!!"
+}
+
+func (serv MsgService) GetMessage() string {
+	return "Welcome to Message Story"
+}
+
+func (serv MsgService) GetLogin() string {
+	return Authenticate.Login()
+}
+
+func (serv UserService) GetUser(name string) string {
+	return User.GetUser(name)
+
+	// serv.ResponseBuilder().SetResponseCode(404).Overide(true)
+	// return
+}
+
+func main() {
+	gorest.RegisterService(new(AppService))
+	gorest.RegisterService(new(UserService))
+	gorest.RegisterService(new(MsgService))
+	gorest.RegisterService(new(RegisterService))
+	http.Handle("/", gorest.Handle())
+	http.ListenAndServe(":8080", nil)
+}
