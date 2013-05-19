97d22b6f74ace1c98920d85284dbd11ba72ed27e
Jasdeep Madan <jasdeepm@gmail.com>
2013-05-19 01:35:15 +0100

    refactored login method to return only one value and added tests

--- authenticate/authenticate.go
+++ authenticate/authenticate.go
@@ -3,7 +3,7 @@ package authenticate
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
-	User "github.com/jmadan/go-msgstory/user"
+	// User "github.com/jmadan/go-msgstory/user"
 	"log"
 	"os"
 )
@@ -16,43 +16,37 @@ type Authenticate struct {
 }
 
 //private function to verify credentials with MySQL
-func (a *Authenticate) authorize() (User.User, error) {
-	var person User.User
-	dburl := os.Getenv("CLEARDB_DATABASE_URL")
-
-	// db, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/msgstory")
+func (a *Authenticate) authorize() {
+	// var person User.User
+	dburl := os.Getenv("DATABASE_URL")
+	// "mysql", "root:password@tcp(localhost:3306)/msgstory"
 	db, err := sql.Open("mysql", dburl[8:])
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Fatal("Phat Gayee : " + err.Error())
 	}
 	defer db.Close()
 
 	stmtOut, err := db.Prepare("SELECT USER_ID, USEREMAIL FROM USERS WHERE USEREMAIL = ? AND PASSWORD = ?")
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Fatal("stmtError :" + err.Error())
 	}
 	defer stmtOut.Close()
 
 	// err = stmtOut.QueryRow(useremail, userpassword).Scan(&authorize.user_id, &authorize.email)
-
 	err = stmtOut.QueryRow(a.email, a.password).Scan(&a.user_id, &a.email)
 
 	if err != nil {
-		log.Print(err.Error())
-		log.Println("Not found")
+		log.Print("stmtExecution: " + err.Error())
+		a.isAuthenticated = false
 	} else {
 		a.isAuthenticated = true
-		log.Println("Logged In")
-		person, err = User.GetByEmailAndUserId(a.email, a.user_id)
+		log.Println(a.user_id)
 	}
-
-	return person, err
 }
 
 //Login function to check users credentials
-func Login(email, password string) (User.User, error) {
+func Login(email, password string) Authenticate {
 	a := Authenticate{email, password, 0, false}
-	user, err := a.authorize()
-	log.Println(err.Error())
-	return user, err
+	a.authorize()
+	return a
 }
--- authenticate/authenticate_test.go
+++ authenticate/authenticate_test.go
@@ -7,24 +7,32 @@ import (
 func Test_Authorize_Incorrect_Credentials(t *testing.T) {
 
 	authenticate := Authenticate{"a@a.com", "something", 0, false}
-	user, err := authenticate.authorize()
-	if err != nil {
-		t.Log("PASSED")
-		t.Log(user.Email)
-	} else {
+	authenticate.authorize()
+	if authenticate.isAuthenticated {
 		t.Log("Test_Authorize_Incorrect_Credentials Failed")
 		t.Fail()
+	} else {
+		t.Log("Test_Authorize_Incorrect_Credentials PASSED")
 	}
 }
 
 func Test_Authorize_Correct_Credentials(t *testing.T) {
 	authenticate := Authenticate{"jasdeepm@gmail.com", "98036054", 0, false}
-	user, err := authenticate.authorize()
-	if err == nil {
+	authenticate.authorize()
+	if authenticate.isAuthenticated {
 		t.Log("PASSED")
-		t.Log(user.Email)
 	} else {
 		t.Log("Test_Authorize_Correct_Credentials Failed")
+		t.Fail()
+	}
+}
+
+func Test_Login_Method(t *testing.T) {
+	auth := Login("jasdeepm@gmail.com", "98036054")
+	if auth.isAuthenticated {
+		t.Log("Login Method PASSED")
+	} else {
+		t.Log("Login Method FAILED")
 		t.Fail()
 	}
 }
