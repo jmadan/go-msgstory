9b8a1caab4ba3b30a16fd14c1e4269dc0be5469f
Jasdeep Madan <jasdeepm@gmail.com>
2013-06-02 19:38:23 +0100

    adding test file

--- geolocation/geolocation_test.go
+++ geolocation/geolocation_test.go
@@ -1,23 +1,32 @@
 package geolocation
 
 import (
+	"fmt"
+	SJ "github.com/bitly/go-simplejson"
 	"testing"
 )
 
 func Test_GetVenuesWithLatitudeAndLongitude(t *testing.T) {
-	location := GeoLocation{}
-	resbody, status_code := location.GetVenuesWithLatitudeAndLongitude("53.48", "2.24")
-	if status_code == "200" {
-		t.Log("Passed")
+	// location := GeoLocation{}
+	resbody := GetVenuesWithLatitudeAndLongitude("37.422005", "-122.084095")
+	js, err := SJ.NewJson([]byte(resbody))
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
 	}
-	t.Log(resbody)
+	arr, _ := js.Get("meta").Get("code").Int()
+	if arr == 200 {
+		t.Log("Test_GetVenuesWithLatitudeAndLongitude PASSED")
+	} else {
+		t.Fail()
+	}
 }
 
-func Test_GetVenues(t *testing.T) {
-	location := GeoLocation{}
-	res := location.GetVenues("Manchester")
-	if res == "200" {
-		t.Log("Passed")
-	}
+// func Test_GetVenues(t *testing.T) {
+// 	location := GeoLocation{}
+// 	res := location.GetVenues("Manchester")
+// 	if res == "200" {
+// 		t.Log("Passed")
+// 	}
 
-}
+// }
