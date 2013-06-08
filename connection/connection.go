baae051d6a4dcde63e7efccc615dbbc8291deba4
Jasdeep Madan <jasdeepm@gmail.com>
2013-06-08 18:37:42 +0100

    extracting db connection

--- connection/connection.go
+++ connection/connection.go
@@ -2,6 +2,8 @@ package connection
 
 import (
 	"labix.org/v2/mgo"
+	"log"
+	"os"
 )
 
 var (
@@ -12,9 +14,9 @@ var (
 func GetDBSession() *mgo.Session {
 	if mgoSession == nil {
 		var err error
-		mgoSession, err = mgo.Dial("localhost")
+		mgoSession, err = mgo.Dial(os.Getenv("MONGOHQ_URL"))
 		if err != nil {
-			panic(err) // no, not really
+			log.Println(err) // no, not really
 		}
 	}
 	return mgoSession.Clone()
@@ -26,3 +28,23 @@ func WithCollection(collection string, s func(*mgo.Collection) error) error {
 	c := dbSession.DB(databaseName).C(collection)
 	return s(c)
 }
+
+// func ExecQuery (collection string, q interface{}, skip int, limit int) (searchResults []string, searchErr string) {
+//     searchErr     = ""
+//     searchResults = []Person{
+//     query := func(c *mgo.Collection) error {
+//         fn := c.Find(q).Skip(skip).Limit(limit).All(&searchResults)
+//         if limit < 0 {
+//             fn = c.Find(q).Skip(skip).All(&searchResults)
+//         }
+//         return fn
+//     }
+//     search := func() error {
+//         return withCollection("person", query)
+//     }
+//     err := search()
+//     if err != nil {
+//         searchErr = "Database Error"
+//     }
+//     return
+// }
