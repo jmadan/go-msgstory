4dee9e23f002626fb1cbed63859b9b363beea984
Jasdeep Madan <jasdeepm@gmail.com>
2013-06-16 11:29:56 +0100

    added status and error fields

--- util/returndata.go
+++ util/returndata.go
@@ -2,7 +2,9 @@ package util
 
 type ReturnData struct {
 	Success  bool   `json:"success"`
+	ErrorMsg error  `json:"error_message"`
 	JsonData string `json:"json_data"`
+	Status   string `json:"status"`
 }
 
 func (return_data *ReturnData) GetSuccess() bool {
@@ -11,4 +13,12 @@ func (return_data *ReturnData) GetSuccess() bool {
 
 func (return_data *ReturnData) GetJsonData() string {
 	return return_data.JsonData
+}
+
+func (return_data *ReturnData) GetErrorMessage() error {
+	return return_data.ErrorMsg
+}
+
+func (return_data *ReturnData) GetStatus() string {
+	return return_data.Status
 }
